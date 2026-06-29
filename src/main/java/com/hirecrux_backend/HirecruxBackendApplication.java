f4f8c0c41904afe4c1fe4349b0f60289900f7a8b
monikadhana <monika124monika@gmail.com>
2026-06-29 14:45:43 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/hirecrux_backend/HirecruxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hirecrux_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirecruxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirecruxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hirecrux_backend/HirecruxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hirecrux_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HirecruxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
