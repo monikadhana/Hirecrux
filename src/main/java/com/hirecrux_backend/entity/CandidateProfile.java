9a97b94b052590da7d73f25785e4033ce6e31c69
Monika <105918200+monikadhana@users.noreply.github.com>
2026-07-01 20:48:52 +0530

    Update CandidateProfile.java

--- src/main/java/com/hirecrux_backend/entity/CandidateProfile.java
+++ src/main/java/com/hirecrux_backend/entity/CandidateProfile.java
@@ -1,20 +1,43 @@
 package com.hirecrux_backend.entity;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
+import java.time.LocalDateTime;
 import lombok.Data;
 
 @Entity
 @Data
+@Table(name="candidate_profile")
 public class CandidateProfile {
+    @Id
+    @Column(name = "candidate_id")
+    private Integer candidateId;
+
     @OneToOne
-    @JoinColumn(name = "candidate_id")
+    @JoinColumn(name = "candidate_id", insertable = false, updatable = false)
     private User user;
 
-    private String resume_url;
+    @Column(name="resume_url", length= 500)
+    private String resumeUrl;
 
-    private Integer total_experience;
-    
+    @Column(name= "total_experience", nullable = false)
+    private BigDecimal totalExperience;
 
+    @Column(name= "education", nullable = false)
+    private String education;
+
+    @Column(name= "linkedin_url", nullable=false, length= 255)
+    private String linkedinUrl;
+
+    @Column(name= "github_url", nullable=false, length= 255)
+    private String githubUrl;
+
+    @Column(name= "created_at", insertable=false, updatable= false)
+    private LocalDateTime createdAt;
+
+    @Column(name= "updated_at", insertable=false, updatable= false)
+    private LocalDateTime updatedAt;
 }
